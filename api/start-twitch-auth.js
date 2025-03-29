649461d4fa6f5db3a98cc52072b98535c691349c
Jim Liu <jim.liu.95@gmail.com>
2025-03-29 13:26:26 -0400

    Test basic endpoint

--- api/start-twitch-auth.js
+++ api/start-twitch-auth.js
@@ -7,6 +7,7 @@ export default function handler(req, res) {
   
     const twitchUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirect}&response_type=code&scope=${scope}&state=${session}`;
   
+    res.status(200).send("Start Twitch Auth endpoint is live!");
     res.redirect(twitchUrl);
   }
   
